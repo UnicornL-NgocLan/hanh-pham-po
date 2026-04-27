42b53a8f15454fc4f780ed6bfe8dd943b0f3673d
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2026-04-27 20:14:33 +0700

    feat: initialize express server with database connection and route configurations

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(helmet())
